8e97892ce55ce630e89a8d7a607dbcb07a333fd8
VIVEK DHIMAN <53940939+DHIMANvivek@users.noreply.github.com>
2023-10-02 19:57:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ require("./config/db/db");
 
 // Set up CORS headers
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200' , 'https://e-commerce-backend-7tt3.vercel.app', 'https://e-commerce-backend-7tt3-i7ilppj5a-dhimanvivek.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE,FETCH');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
