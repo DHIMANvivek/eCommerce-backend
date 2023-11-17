600f26c8ea597868bc2c81896e773a02f527f869
Vivek Dhiman <vivek.dhiman@venturepact.com>
2023-11-17 11:23:39 +0000

    Update index.js

--- config/routes/index.js
+++ config/routes/index.js
@@ -76,7 +76,7 @@ router.post('/sendMail', async (req, res) => {
 // payment intent
 router.post('/create-payment-intent', async (req, res) => {
 
-    const response = await fetch('http://localhost:1000/paymentKeys/get');
+    const response = await fetch('https://tradevogue-backend.onrender.com/paymentKeys/get');
 
     if (!response.ok) {
         throw new Error('Network response was not ok');
