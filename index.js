519763578eb1aace59f8bf9a4dcc3888b96a9b84
anorangefalcon <priyanshudhall2001@gmail.com>
2023-11-17 12:16:47 +0530

    re-build cors

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ require("./config/db/db");
 
 // Set up CORS headers
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200', 'https://tradevogue-backend.onrender.com');
+  res.setHeader('Access-Control-Allow-Origin', 'https://tradevogue-backend.onrender.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE,FETCH');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
