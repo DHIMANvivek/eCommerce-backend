6df0b8b5f74172c1ba1bcba0176736266b32b93d
Vivek Dhiman <vivekdhiman@Viveks-MacBook-Air-3.local>
2023-12-06 16:56:06 +0530

    solved

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ require("./config/db/db");
 
 // Set up CORS headers
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://tradevogue.web.app', 'http://localhost:4200');
+  res.setHeader('Access-Control-Allow-Origin', '*', 'https://tradevogue.web.app', 'http://localhost:4200');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE,FETCH');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
